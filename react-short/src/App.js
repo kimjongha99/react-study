a8ee36d6eda9a9d08e3555c1a52c9c5938e96d56
kimjongha99 <jjhsun99@gmail.com>
2023-10-23 23:34:44 +0900

    todo test

---
+++ react-short/src/App.js
@@ -0,0 +1,51 @@
+import './App.css';
+import {useState} from "react";
+
+function App() {
+  const  [todo , setTodo ]= useState([
+
+
+  ]);
+  const  [newTodo, setNewTodo] = useState('');
+
+    const  onChangeNewTodo = (e) =>{
+    setNewTodo(e.target.value);
+}
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        setTodo(prevTodos => [
+            ...prevTodos,
+            { title: newTodo, completed: false, id: Math.random() }
+        ]);
+        setNewTodo(''); // Clearing the input field after submitting
+    }
+
+    return (
+        <div className="App">
+            <h1>Todo List</h1>
+            <ul className="todo-list">
+                {todo.length === 0 && (
+                    <div>
+                        <div>할일이 없습니다</div>
+                    </div>
+                )}
+
+
+                {todo.map((item) => (
+                    <li key={item.id}>
+                        {item.title}
+                    </li>
+                ))}
+
+
+            </ul>
+            <form onSubmit={onSubmit}>
+                <div>할일을 추가하세요</div>
+                <input type="text" value={newTodo} onChange={onChangeNewTodo} />
+                <button type="submit">추가</button>
+            </form>
+        </div>
+    );
+}
+export default App;
